000e3b6d46e7670f9e753705068973f9992dfc68
chrimp <jaesook719@gmail.com>
2024-10-26 12:38:50 +0900

    Initial Commit

---
+++ Fitness-Like-HKRing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitness-Like-HKRing
+//
+//  Created by chrimp on 10/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitness-Like-HKRing/Fitness_Like_HKRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_Like_HKRingApp.swift
+//  Fitness-Like-HKRing
+//
+//  Created by chrimp on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_Like_HKRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
